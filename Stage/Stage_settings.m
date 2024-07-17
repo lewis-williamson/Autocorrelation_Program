07d5868453f270644dfdf1942966adb2824a324b
lewis-williamson <lewiswilliamson973@googlemail.com>
2024-07-17 15:18:41 +0100

    moved files around

---
+++ Stage/Stage_settings.m
@@ -0,0 +1,29 @@
+function    []= Stage_settings(S)
+
+S= S_Structure;
+set([S.pb1,S.pb2,S.pb3,S.pb4,S.pb5],'callback',{@run_stage,S});
+    
+    function []=run_stage(varargin)
+   serial_num='97101961';
+        n=0;
+    [device,channelsEnums,jogDirectionEnums,timeout_val]=Stage_connection(serial_num,n)
+    n=1
+
+        if varargin{1}==O.pb1                                   %button for selecting the timebase
+               str2 = get(O.pb2,{'str','value'});
+       
+                switch str2{1}
+                        
+                    case '97101961'
+               
+                        [device,channelsEnums,jogDirectionEnums,timeout_val]=Stage_connection(serial_num,n)
+
+                end
+        end 
+
+
+[device,channelsEnums,jogDirectionEnums,timeout_val]=Stage_connection(serial_num,n)
+
+
+    end
+end
