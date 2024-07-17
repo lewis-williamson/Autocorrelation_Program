2f5daf9d2e6a8a329cb6aee0556ca94281612377
lewis-williamson <lewiswilliamson973@googlemail.com>
2024-07-17 15:17:56 +0100

    Moves some files around

--- Stage_connection.m
+++ Stage_connection.m
@@ -1,90 +1,10 @@
-function [device,channelsEnums,jogDirectionEnums,timeout_val]=Stage_connection(serial_num)
+function [device,channelsEnums,jogDirectionEnums,timeout_val]=Stage_connection(serial_num,n)
+
 %both check connection and if not connected then connect to the stage
-
 %check if device is connected
-
- try 
-       selftest(deviceObj)             %returns an error if not connected if not 
+if n==0 
+      % selftest(device)             %returns an error if not connected if not if has gone through initalisation thent this will skip
+       [device,channelsEnums,jogDirectionEnums,timeout_val]=run_connection
+else
         
-    catch
-        connect(deviceObj)
-    end 
-
-devCLI = NET.addAssembly('C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.DeviceManagerCLI.dll');
-genCLI = NET.addAssembly('C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.GenericMotorCLI.dll');
-motCLI = NET.addAssembly('C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.KCube.InertialMotorCLI.dll');
-
-import Thorlabs.MotionControl.DeviceManagerCLI.*
-import Thorlabs.MotionControl.GenericMotorCLI.*
-import Thorlabs.MotionControl.KCube.InertialMotorCLI.*
-
-%% Create Simulation (Comment out for real device)
-% 
-
-%% Connect
-% Builds Device list
-DeviceManagerCLI.BuildDeviceList();
-
-% Will need to update serial number to correct device
-serial_num='97101961';
-timeout_val=60000;
-
-%Connect to controller
-device = KCubeInertialMotor.CreateKCubeInertialMotor(serial_num);
-device.Connect(serial_num);
-device.WaitForSettingsInitialized(5000);
-
-device.StartPolling(250);
-device.EnableDevice();
-pause(1) %wait to make sure device is enabled
-devCLI = NET.addAssembly('C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.DeviceManagerCLI.dll');
-genCLI = NET.addAssembly('C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.GenericMotorCLI.dll');
-motCLI = NET.addAssembly('C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.KCube.InertialMotorCLI.dll');
-
-import Thorlabs.MotionControl.DeviceManagerCLI.*
-import Thorlabs.MotionControl.GenericMotorCLI.*
-import Thorlabs.MotionControl.KCube.InertialMotorCLI.*
-
-%% Create Simulation (Comment out for real device)
-% 
-
-%% Connect
-% Builds Device list
-DeviceManagerCLI.BuildDeviceList();
-
-% Will need to update serial number to correct device
-serial_num='97101961';
-timeout_val=60000;
-
-%Connect to controller
-device = KCubeInertialMotor.CreateKCubeInertialMotor(serial_num);
-%device.Connect(serial_num);
-device.WaitForSettingsInitialized(5000);
-
-device.StartPolling(250);
-device.EnableDevice();
-pause(1) %wait to make sure device is enabled
-
-% Pull the Enums needed
-channelsHandle = motCLI.AssemblyHandle.GetType('Thorlabs.MotionControl.KCube.InertialMotorCLI.InertialMotorStatus+MotorChannels');
-
-channelsEnums = channelsHandle.GetEnumValues();
-jogDirectionHandle = motCLI.AssemblyHandle.GetType('Thorlabs.MotionControl.KCube.InertialMotorCLI.InertialMotorJogDirection');
-jogDirectionEnums = jogDirectionHandle.GetEnumValues();
-
-% Zero the actuator
-fprintf("Zero the actuator\n")
-device.SetPositionAs(channelsEnums.GetValue(0), 0);
-device.Jog(channelsEnums.GetValue(0), jogDirectionEnums.GetValue(0), timeout_val);
-% Pull the Enums needed
-channelsHandle = motCLI.AssemblyHandle.GetType('Thorlabs.MotionControl.KCube.InertialMotorCLI.InertialMotorStatus+MotorChannels');
-
-channelsEnums = channelsHandle.GetEnumValues();
-jogDirectionHandle = motCLI.AssemblyHandle.GetType('Thorlabs.MotionControl.KCube.InertialMotorCLI.InertialMotorJogDirection');
-jogDirectionEnums = jogDirectionHandle.GetEnumValues();
-
-% Zero the actuator
-fprintf("Zero the actuator\n")
-device.SetPositionAs(channelsEnums.GetValue(0), 0);
-device.Jog(channelsEnums.GetValue(0), jogDirectionEnums.GetValue(0), timeout_val);
-end
+end 
--- Stage_settings.m
+++ Stage_settings.m
@@ -1,10 +1,26 @@
 function    []= Stage_settings(S)
 
-S= Stage_Structure;
+S= S_Structure;
+set([S.pb1,S.pb2,S.pb3,S.pb4,S.pb5],'callback',{@run_stage,S});
+    
+    function []=run_stage(varargin)
+   serial_num='97101961';
 
-serial_num='97101961';
+        n=0;
 
-[device,channelsEnums,jogDirectionEnums,timeout_val]=Stage_connection(serial_num)
+        if varargin{1}==S.pb1                                   %button for selecting the timebase
+               str2 = get(S.pb2,{'str','value'});
+       
+                switch str2{1}
+                        
+                    case '97101961'
+               
+                        [device,channelsEnums,jogDirectionEnums,timeout_val]=Stage_connection(serial_num,n);
+                        n=1
+                        
 
+                end
+        end 
 
+    end
 end
