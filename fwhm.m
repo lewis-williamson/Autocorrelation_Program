6a6eb04bae2449442cc983b7f6d6cdcbf8034341
lewis-williamson <lewiswilliamson973@googlemail.com>
2024-08-01 10:56:24 +0100

    Changed fwhm function to sech^2

--- fwhm.m
+++ fwhm.m
@@ -1,29 +1,59 @@
-function [width, center, roots, Yfit]=fwhm(X,Y)
-%fwhm function for gaussian / sech2 pulses. A fit if given to the data with
-%the half max found and a stragiht line intersects the fit at the x,y
-%locations of the fwhm at either side
-format long
-fitobject=fit(X,Y,'gauss2');
-Yfit=feval(fitobject,X);
+function [FWHM,fit]=fwhmsec(x_data,y_data)
+%[width, center, roots, Yfit]
+sech2_model = @(params, x) params(1) * sech(params(2) * (x - params(3))).^2 + params(4);
 
-max_es=find(Yfit==(max(Yfit)));                   %find locations with max values 
+% Initial guess for the parameters [amplitude, scale, center]
+initial_guess = [max(y_data), 1, mean(x_data), min(y_data),1];
 
-if mod(length(max_es),2)==1;                %for odd numners the result will be 1 
-    center=max_es((length(max_es)/2)+0.5);  % therefore for odd numbers, the center location is 
-    else
-    center=max_es(length(max_es)/2) ;       %gives the location of the maximum 
-end
+% Set bounds for the parameters if necessary
+lb = [0, 0, min(x_data), -Inf]; % Lower bounds [amplitude, scale, center, offset]
+ub = [Inf, Inf, max(x_data), Inf]; % Upper bounds [amplitude, scale, center, offset]
 
-cenY=((Yfit(center)-min(Yfit))/2)+min(Yfit)  ;      %find half max value 
+% Perform the curve fitting using lsqcurvefit
+options = optimoptions('lsqcurvefit', 'Display', 'iter', 'MaxIterations', 10000);
+[params_fit, resnorm, residual, exitflag, output] = lsqcurvefit(sech2_model, initial_guess, x_data,abs(y_data), lb, ub, options);
+
+fit=sech2_model(params_fit, x_data)
+
+% Display the fitted parameters
+disp('Fitted parameters:');
+disp(params_fit);
 
 
-strt_line=cenY*ones(1,length(X))  ;          %creating straight line to compair wit curve
+%% Analytical FWHM calculation
+half_max = (max(fit) + params_fit(4))/2;
 
-L1=[rot90(vertcat(X)) ; strt_line];
-L2=[rot90(vertcat(X)) ; rot90(vertcat(Yfit))] ;  %linear arrays to be compared
+% Find the indices where the fitted curve crosses the half-maximum value
+above_half_max = fit > half_max;
+cross_points = diff(above_half_max);
 
-r=coeffvalues(fitobject)            ;            %results of comparision 
-format long;
-roots=(InterX(L1,L2));
-width=roots(1,2)-roots(1,1);
-end
+% Find the x-values of the crossings
+x_cross1 = x_data(find(cross_points, 1, 'first'));
+x_cross2 = x_data(find(cross_points, 1, 'last'));
+
+% Calculate the FWHM
+fwhm = x_cross2 - x_cross1;
+
+% Display the FWHM
+disp(['FWHM: ', num2str(fwhm)]);
+
+%% params fit fwhm
+
+FWHM = 2 * log(sqrt(2) + 1) / params_fit(2);
+disp(FWHM);
+%% plot results
+
+% Plot the original data and the fitted curve
+figure;
+hold on;
+scatter(x_data, abs(y_data), 'bo'); % Original data
+plot(x_data, fit, 'r-', 'LineWidth', 2); % Fitted curve
+title('Data Fitting using sech^2 Function');
+xlabel('x');
+ylabel('y');
+legend1=['Params FWHM = ' num2str(FWHM, '%.2f')]
+legend(legend1,['Analytical FWHM = ' num2str(fwhm, '%.2f')],legend1);
+
+hold off;
+
+end 
