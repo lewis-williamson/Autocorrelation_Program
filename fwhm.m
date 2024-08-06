c8f5ba4aed08875c9d92c21a4f627b019c456f4d
lewis-williamson <lewiswilliamson973@googlemail.com>
2024-08-06 10:34:23 +0100

    Changes to get fwhm sech^2 to fit data

--- fwhm.m
+++ fwhm.m
@@ -1,24 +1,30 @@
 function [FWHM,center_x,center_y,fit]=fwhmsec(x_data,y_data)
 
+% Define the sech^2 function to fit with additional scaling parameter
 sech2_model = @(params, x) params(1) * sech(params(2) * (x - params(3))).^2 + params(4);
 
-% Initial guess for the parameters [amplitude, scale, center]
-initial_guess = [max(y_data), 1, mean(x_data), min(y_data),1];
+% Initial guess for the parameters [amplitude, scale, center, offset]
+amplitude_guess = max(y_data) - min(y_data); 
+scale_guess = 1;
+center_guess = mean(x_data); 
+offset_guess = min(y_data); 
 
-% Set bounds for the parameters if necessary
+initial_guess = [amplitude_guess, scale_guess, center_guess, offset_guess];
+
+% Set bounds for the parameters
 lb = [0, 0, min(x_data), -Inf]; % Lower bounds [amplitude, scale, center, offset]
 ub = [Inf, Inf, max(x_data), Inf]; % Upper bounds [amplitude, scale, center, offset]
 
 % Perform the curve fitting using lsqcurvefit
-options = optimoptions('lsqcurvefit', 'Display', 'iter', 'MaxIterations', 10000);
-[params_fit, resnorm, residual, exitflag, output] = lsqcurvefit(sech2_model, initial_guess, x_data,abs(y_data), lb, ub, options);
+options = optimoptions('lsqcurvefit', 'Display', 'iter', 'MaxIterations', 1000, 'TolFun', 1e-9, 'TolX', 1e-9);
+params_fit = lsqcurvefit(sech2_model, initial_guess, x_data, y_data, lb, ub, options);
 
-fit=sech2_model(params_fit, x_data);
 
 % Display the fitted parameters
 disp('Fitted parameters:');
 disp(params_fit);
 
+fit=sech2_model(params_fit, x_data);
 
 %% Analytical FWHM calculation
 half_max = (max(fit) + params_fit(4))/2;
@@ -49,17 +55,17 @@ center_x=x_data(loc);
 
 %% plot results
 
-% Plot the original data and the fitted curve
-%figure;
-%hold on;
-%scatter(x_data, abs(y_data), 'bo'); % Original data
-%plot(x_data, fit, 'r-', 'LineWidth', 2); % Fitted curve
-%scatter(center_x,center_y);
-% title('Data Fitting using sech^2 Function');
-% xlabel('x');
-% ylabel('y');
-% legend1=['Params FWHM = ' num2str(FWHM, '%.2f')];
-% legend(legend1,['Analytical FWHM = ' num2str(fwhm, '%.2f')],legend1);
+%Plot the original data and the fitted curve
+figure;
+hold on;
+scatter(x_data, abs(y_data), 'bo'); % Original data
+plot(x_data, fit, 'r-', 'LineWidth', 2); % Fitted curve
+scatter(center_x,center_y);
+title('Data Fitting using sech^2 Function');
+xlabel('x');
+ylabel('y');
+legend1=['Params FWHM = ' num2str(FWHM, '%.2f')];
+legend(legend1,['Analytical FWHM = ' num2str(fwhm, '%.2f')],legend1);
 
 hold off;
 
