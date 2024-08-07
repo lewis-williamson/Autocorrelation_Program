d2c0f2a36cd9940598233ca46d11dd74d2823cf2
lewis-williamson <lewiswilliamson973@googlemail.com>
2024-08-07 15:40:19 +0100

    mainly changes to measurement but also added a few scripts to take multiple readings

---
+++ Multiple_Measurements.m
@@ -0,0 +1,20 @@
+[X1_1,Y1_1] = ReadWaveform('channel1');
+pause(1)
+[X1_2,Y1_2] = ReadWaveform('channel1');
+pause(1)
+[X1_3,Y1_3] = ReadWaveform('channel1');
+pause(1)
+[X1_4,Y1_4] = ReadWaveform('channel1');
+pause(1)
+[X1_5,Y1_5] = ReadWaveform('channel1');
+pause(1)
+[X1_6,Y1_6] = ReadWaveform('channel1');
+pause(1)
+[X1_7,Y1_7] = ReadWaveform('channel1');
+pause(1)
+[X1_8,Y1_8] = ReadWaveform('channel1');
+pause(1)
+[X1_9,Y1_9] = ReadWaveform('channel1');
+pause(1)
+[X1_10,Y1_10] = ReadWaveform('channel1');
+
---
+++ Multiple_measurements_2.m
@@ -0,0 +1,19 @@
+[X2_1,Y2_1] = ReadWaveform('channel1');
+pause(1)
+[X2_2,Y2_2] = ReadWaveform('channel1');
+pause(1)
+[X2_3,Y2_3] = ReadWaveform('channel1');
+pause(1)
+[X2_4,Y2_4] = ReadWaveform('channel1');
+pause(1)
+[X2_5,Y2_5] = ReadWaveform('channel1');
+pause(1)
+[X2_6,Y2_6] = ReadWaveform('channel1');
+pause(1)
+[X2_7,Y2_7] = ReadWaveform('channel1');
+pause(1)
+[X2_8,Y2_8] = ReadWaveform('channel1');
+pause(1)
+[X2_9,Y2_9] = ReadWaveform('channel1');
+pause(1)
+[X2_10,Y2_10] = ReadWaveform('channel1');
